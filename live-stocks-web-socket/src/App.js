25b62f447e8c59b4633be7b5db6f0ed778401b77
Sri Ram Pavan Amajala <sxa141101@darden.com>
2024-05-24 11:57:09 +0530

    static live using some random number generation using web socket

---
+++ live-stocks-web-socket/src/App.js
@@ -0,0 +1,14 @@
+import StockPrice from './StockPrice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StockPrice/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
